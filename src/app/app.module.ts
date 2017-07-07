8087ada7a4fe9b01003e010185e69527975827d2
ricardo luiz <ricardoluiz508@gmail.com>
2017-07-06 21:53:05 -0300

    Adicionado o Mapa
    Coloquei no fundo do home o mapa do google maps. Ja tem um icone personalizado e coloca um marcador em cima do device (nao estou fazendo tracking da position)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,9 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { Geolocation } from '@ionic-native/geolocation';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
 import { HttpModule } from '@angular/http';
 import { HomeProvider } from '../providers/home/home';
 
@@ -32,7 +31,8 @@ import { HomeProvider } from '../providers/home/home';
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    HomeProvider
+    HomeProvider,
+    Geolocation
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,10 @@
+import { Geolocation } from '@ionic-native/geolocation';
 import { HomeProvider } from './../../providers/home/home';
-import { Component } from '@angular/core';
+import { Component,ViewChild,ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { MenuController } from 'ionic-angular';
+
+declare var google;
 
 @Component({
   selector: 'page-home',
@@ -9,17 +12,78 @@ import { MenuController } from 'ionic-angular';
 })
 export class HomePage {
 
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
   categorias: Array<any>;
 
   constructor(public navCtrl: NavController,
               public menuCtrl: MenuController,
-              public homeProvider: HomeProvider) {
+              public homeProvider: HomeProvider,
+              public geolocation: Geolocation) {
 
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TarefaListPage');    
     this.recuperarCategorias();
+    this.loadMap();
+  }
+
+  loadMap(){
+    this.geolocation.getCurrentPosition().then((position) => {
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP        
+      }
+
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+      this.adicionarPosicaoAtual(latLng);
+    }, (err) => {
+      console.log(err);
+    });    
+  }
+
+  //Pega a posicao atual do device
+  adicionarPosicaoAtual(latLng){    
+
+    let marcador = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: latLng,
+      icon: {url : 'imagem/myPositionIcon.png'},
+    });
+
+    let conteudo = "<h4>Você esta aqui</h4>";
+
+    this.adicionarJanelaInformacao(marcador,conteudo); 
+  }
+
+  //Coloca posições customizadas
+  adicionarMarcador(){
+
+    let marcador = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
+
+    let conteudo = "<h4>Informação</h4>";
+
+    this.adicionarJanelaInformacao(marcador,conteudo); 
+  }
+
+  adicionarJanelaInformacao(marker, content){
+    let infoWindow = new google.maps.InfoWindow({
+    content: content
+    });
+  
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
   }
 
   recuperarCategorias(){
